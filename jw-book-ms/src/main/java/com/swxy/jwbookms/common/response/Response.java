0253b976286195cf43d213d2061648cca72ed9e3
chang <1584541746@qq.com>
2021-01-16 12:03:32 +0800

    修改

--- jw-book-ms/src/main/java/com/swxy/jwbookms/common/response/Response.java
+++
@@ -1,9 +0,0 @@
-package com.swxy.jwbookms.common.response;
-
-import io.swagger.annotations.ApiModel;
-
-@ApiModel(value = "公共返回接口")
-public interface Response {
-    public static final boolean SUCCESS = true;
-    public static final int SUCCESS_CODE = 10000;
-}
