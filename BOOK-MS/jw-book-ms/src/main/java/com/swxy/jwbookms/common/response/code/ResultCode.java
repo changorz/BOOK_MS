45d5f22d1a3b70c03f2e626f627811f194c36901
chang <1584541746@qq.com>
2021-01-16 12:08:17 +0800

    表格的基本增删改查

--- BOOK-MS/jw-book-ms/src/main/java/com/swxy/jwbookms/common/response/code/ResultCode.java
+++ BOOK-MS/jw-book-ms/src/main/java/com/swxy/jwbookms/common/response/code/ResultCode.java
@@ -9,13 +9,13 @@ package com.swxy.jwbookms.common.response.code;
  * 25000-- 文件系统
  */
 public interface ResultCode {
-    //操作是否成功,true为成功，false操作失败
+    // 操作是否成功,true为成功，false操作失败
     boolean success();
 
-    //操作代码
+    // 操作代码
     int code();
 
-    //提示信息
+    // 提示信息
     String message();
 
 
